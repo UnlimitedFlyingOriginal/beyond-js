c2bba84d8eb5c43f04629d95c19aa658f4672fb4
Gihyeon <ken29511@gmail.com>
2021-08-12 23:49:47 +0900

    PRACTICE: practice1, practice2-1 풀이

---
+++ gihyeon/practice/practice2-1_보충수업2/app.js
@@ -0,0 +1,32 @@
+function inputScores() {
+  const korean = isNaN(parseInt(prompt("국어점수를 입력해주세요.")))
+    ? 0
+    : korean;
+  const english = isNaN(parseInt(prompt("영어점수를 입력해주세요.")))
+    ? 0
+    : english;
+  const math = isNaN(parseInt(prompt("수학점수를 입력해주세요."))) ? 0 : math;
+  const scores = [korean, english, math];
+
+  return scores;
+}
+
+function checkMakeUpClass(scores) {
+  if (scores[0] <= 50 && scores[1] <= 50 && scores[2] <= 50) {
+    console.log("종합반");
+  } else if (scores[0] <= 50 || scores[1] <= 50 || scores[2] <= 50) {
+    console.log("단과반");
+  } else {
+    console.log("보충 수업 없음");
+  }
+}
+
+function init() {
+  const scores = inputScores();
+
+  if (scores.length > 0) {
+    checkMakeUpClass(scores);
+  }
+}
+
+init();

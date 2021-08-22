6e474625a4f7bf99a3b3bc33c7f2476591152159
Gihyeon KIM <ken29511@gmail.com>
2021-08-22 13:48:35 +0900

    remote gihyeon 파일정리

--- gihyeon/practice/practice1_보충수업1/app.js
+++ gihyeon/practice/practice1_보충수업1/app.js
@@ -1,4 +1,3 @@
-
 /*
 ## 보충수업1
 
@@ -20,7 +19,7 @@ function inputScores() {
   math = isNaN(math) ? 0 : math;
 
   let scores = [korean, english, math];
-  
+
   return scores;
 }
 
--- hjlee1811/practice/practice1.js
+++
@@ -1,6 +0,0 @@
-function average(kor, eng, math) {
-  const avg = (kor + eng + math) / 3.0;
-  if (avg <= 50) {
-    console.log('보충수업 대상자 입니다.');
-  } 
-}

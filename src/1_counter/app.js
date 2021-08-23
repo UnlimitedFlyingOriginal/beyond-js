fac11714d0f79512f433f1a80f1a879a69b6fba9
Gihyeon KIM <ken29511@gmail.com>
2021-08-23 23:53:12 +0900

    add: counter code HTML, JS

--- src/1_counter/app.js
+++ src/1_counter/app.js
@@ -1,2 +1,25 @@
-document.getElementById('app').innerHTML = `
-<h1>Hello Vanilla!</h1>`;
+const $result = document.querySelector("#result");
+let result = Number($result.textContent);
+
+const onClickButton = (event) => {
+  switch (event.target.textContent) {
+    case "+":
+      result += 1;
+      $result.textContent = result;
+      break;
+    case "reset":
+      result = 0;
+      $result.textContent = result;
+      break;
+    case "-":
+      result -= 1;
+      $result.textContent = result;
+      break;
+    default:
+      break;
+  }
+};
+
+document.querySelector("#increase").addEventListener("click", onClickButton);
+document.querySelector("#reset").addEventListener("click", onClickButton);
+document.querySelector("#decrease").addEventListener("click", onClickButton);

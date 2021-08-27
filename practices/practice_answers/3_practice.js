c6927235d76eaf8a05581bd960560f3d40017371
Gihyeon KIM <ken29511@gmail.com>
2021-08-27 23:52:10 +0900

    resolve: 3_practice.js 구구단 문자열에 template literal 적용

--- practices/practice_answers/3_practice.js
+++ practices/practice_answers/3_practice.js
@@ -2,14 +2,14 @@
 //1. **홀수단**인 구구단만 출력해보자.
 for (let i = 1; i < 10; i += 2) {
   for (let j = 1; j < 10; j++) {
-    console.log(i + " * " + j + " = " + i * j);
+    console.log(`${i} * ${j} = ${i * j}`);
   }
 }
 //2. 구구단을 가로로 출력해보자.
 for (let i = 1; i < 10; i++) {
   let str = "";
   for (let j = 1; j < 10; j++) {
-    str += i + " * " + j + " = " + i * j + ", ";
+    str += `${i} * ${j} = ${i * j}, `;
   }
   console.log(str);
 }

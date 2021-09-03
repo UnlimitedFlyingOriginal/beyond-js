b62b6807e6a6e5b4ff01a24ca1da6533cd11edce
Gihyeon KIM <ken29511@gmail.com>
2021-09-04 00:18:05 +0900

    fix: 2_practice.js esc key를 누를 경우 프로그램이 종료되도록 수정

--- practices/practice_answers/2_practice.js
+++ practices/practice_answers/2_practice.js
@@ -5,15 +5,17 @@ function getRandomNumber(num1, num2) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-{
+const init = () => {
   alert(
     "두 숫자를 입력해주세요.\n(첫번째 숫자는 두번째 숫자보다 작아야합니다.)"
   );
   let num1;
-  let flag1 = false;
   let message1 = "첫번째 숫자를 입력해주세요.";
-  while (!flag1) {
+  while (true) {
     num1 = prompt(message1);
+    if (num1 === null) {
+      return init;
+    }
     if (num1 !== "" && !isNaN(num1)) {
       num1 = Number(num1);
       break;
@@ -21,10 +23,12 @@ function getRandomNumber(num1, num2) {
     message1 = "첫번째 숫자를 입력해주세요.\n(숫자를 입력해주세요.)";
   }
   let num2;
-  let flag2 = false;
   let message2 = "두번째 숫자를 입력해주세요.";
-  while (!flag2) {
-    num2 = Number(prompt(message2));
+  while (true) {
+    num2 = prompt(message2);
+    if (num2 === null) {
+      return init;
+    }
     if (num1 !== "" && !isNaN(num1) && num1 < num2) {
       num2 = Number(num2);
       break;
@@ -33,7 +37,9 @@ function getRandomNumber(num1, num2) {
       "두번째 숫자를 입력해주세요.\n(첫번째 숫자보다 큰 숫자를 입력해주세요.)";
   }
   console.log(getRandomNumber(num1, num2));
-}
+};
+
+init();
 
 //## 확장자 구별 함수 만들기
 function checkFileExtension(fileExtension) {

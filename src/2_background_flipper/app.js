bcf2aa9cc45fd9e588b37dc94b50f5ff985a528c
Gihyeon KIM <ken29511@gmail.com>
2021-09-06 21:02:36 +0900

    add: 2_background_flipper

--- src/2_background_flipper/app.js
+++ src/2_background_flipper/app.js
@@ -1,2 +1,20 @@
-document.getElementById('app').innerHTML = `
-<h1>Hello Vanilla!</h1>`;
+const [$tomato, $dodgerblue, $hotpink, $yellowgreen, $chocolate, $reset] =
+  document.querySelectorAll("button");
+const $background = document.querySelector(".background");
+
+const onClickButton = (event) => {
+  let color = event.target.textContent;
+
+  if (color === "RESET") {
+    $background.style.background = "white";
+  } else {
+    $background.style.background = color;
+  }
+};
+
+$tomato.addEventListener("click", onClickButton);
+$dodgerblue.addEventListener("click", onClickButton);
+$hotpink.addEventListener("click", onClickButton);
+$yellowgreen.addEventListener("click", onClickButton);
+$chocolate.addEventListener("click", onClickButton);
+$reset.addEventListener("click", onClickButton);

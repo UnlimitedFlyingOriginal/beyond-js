d0d4e748550fcbfb1c708d2c0a6c2e1c11ff4e92
hjlee1811 <hjlee1811@gmail.com>
2021-10-12 00:21:23 +0900

    background flipper commit

--- src/2_background_flipper/app.js
+++ src/2_background_flipper/app.js
@@ -1,2 +1,24 @@
-document.getElementById('app').innerHTML = `
-<h1>Hello Vanilla!</h1>`;
+const $body = document.querySelector("body");
+const $buttons = document.querySelectorAll("button");
+const $reset = document.querySelector(".reset");
+const colors = ["tomato", "blue", "pink", "green", "gold"];
+const resetColor = "white";
+
+function ChangeHandler(event) {
+  for (let i = 0; i < $buttons.length; i++) {
+    if ($buttons[i].textContent === event.target.textContent) {
+      $body.style.backgroundColor = colors[i];
+      $buttons[i].style.backgroundColor = 'white';
+    }
+  }
+  if ($reset.textContent === event.target.textContent) {
+    $body.style.backgroundColor = resetColor;
+  }
+}
+
+for (let i = 0; i < $buttons.length; i++) {
+  $buttons[i].addEventListener("click", ChangeHandler);
+}
+$reset.addEventListener("click", ChangeHandler);
+
+

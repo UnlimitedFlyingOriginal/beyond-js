3dfac79b00e7ea1e3841e3d7b90b15e6911337d2
jjanmo <jjanmo07@hanmail.net>
2021-10-31 23:48:20 +0900

    feat: change calculating logic

--- solutions/4_calculator/app.js
+++ solutions/4_calculator/app.js
@@ -1,79 +1,58 @@
 const $buttons = document.querySelector('.buttons');
 const $screen = document.querySelector('.screen');
 
-let number1 = 0; // 첫번째 숫자
-let number2 = 0; // 두번째 숫자
-let type = ''; // 연산자
-let result = 0; // 계산결과 값
-let text = '0'; // 스크린에 찍히는 문자열
+let result = 0;
+let expression = ['0'];
 
-function calculate(type, number1, number2) {
-  console.log(type, typeof number1);
+function calculate() {
+  const [first, type, second] = expression;
+
   switch (type) {
     case '+': {
-      result = number1 + number2;
-      console.log(result);
-      number1 = result;
-      return result;
+      return Number(first) + Number(second);
     }
     case '-': {
-      result = number1 - number2;
-      number1 = result;
-      return result;
+      return Number(first) - Number(second);
     }
     case '×': {
-      result = number1 * number2;
-      number1 = result;
-      return result;
+      return Number(first) * Number(second);
     }
     case '÷': {
-      result = Number((number1 / number2).toFixed(4));
-      number1 = result;
-      return result;
+      return Number((Number(first) / Number(second)).toFixed(2));
     }
     default: {
+      return;
     }
   }
 }
 
-function checkNumber(number) {
-  if (String(number).length > 4) alert('숫자는 네 자리까지만 입력 가능합니다!');
-}
-
 function renderScreen() {
-  $screen.textContent = text;
+  $screen.textContent = expression.join('');
 }
 
 function handleClickButtons(e) {
   const { className, textContent } = e.target;
 
-  if (className.includes('zero')) {
-    if (text !== '0') text += textContent;
-  } else if (className.includes('number')) {
-    if (text === '0') {
-      number1 = Number(textContent);
-      text = textContent;
+  if (className.includes('number')) {
+    if (expression[0] === '0') {
+      expression.pop();
+      expression.push(textContent);
+    } else if (expression.length === 1 || expression.length === 3) {
+      let prev = expression.pop();
+      prev += textContent;
+      expression.push(prev);
     } else {
-      text += textContent;
-      if (number2 === 0) {
-        checkNumber(number1);
-        number1 = Number(number1 + textContent);
-      } else {
-        checkNumber(number2);
-        number2 = Number(number2 + textContent);
-      }
+      expression.push(textContent);
     }
   } else if (className.includes('operator')) {
-    text += textContent;
-    type = textContent;
-    number = 0;
-  } else if (className.includes('clear')) {
-    text = '0';
-  } else if (className.includes('dot')) {
+    expression.push(textContent);
   } else if (className.includes('equal')) {
-    const result = calculate(type, number1, number2);
-    console.log(result);
-    text = result;
+    if (expression.length === 3) {
+      const result = calculate();
+      expression = [result];
+    }
+  } else if (className.includes('clear')) {
+    expression = ['0'];
   }
 
   renderScreen();
